0b7634b0678944ea68fbc056b6e37e08e9e62038
MatveyKD <97842332+MatveyKD@users.noreply.github.com>
2023-04-26 21:20:35 +0300

    Add buttons

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-import logging
 import os
 
+import telegram
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from dotenv import load_dotenv
 
 
 def start(update: Update, context: CallbackContext) -> None:
+    custom_keyboard = [["Новый вопрос", "Сдаться"],
+                       ["Мой счёт"]]
+    reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
     user = update.effective_user
     update.message.reply_markdown_v2(
         fr'Hi {user.mention_markdown_v2()}\!',
-        reply_markup=ForceReply(selective=True),
+        reply_markup=reply_markup,
     )
 
 
