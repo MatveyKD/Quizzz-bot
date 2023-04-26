35d9f8ff7d3521b9d697af0ddf726dfc8b789d2d
MatveyKD <97842332+MatveyKD@users.noreply.github.com>
2023-04-26 21:36:37 +0300

    Add sending questions

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 import os
 
 import telegram
+import random
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from dotenv import load_dotenv
 
 
-def start(update: Update, context: CallbackContext) -> None:
+def collect_questions(questions_file_path):
+    with open(questions_file_path, 'r', encoding='KOI8-R') as file:
+        file_content = file.read()
+    splited_file = file_content.split('\n\n')[3:]
+    questions_data = dict()
+    question = ""
+    for part in splited_file:
+        sentence = (part.replace('\n', '', 1)).replace('\n', ' ')
+        if sentence.startswith('Вопрос'):
+            question = ' '.join(sentence.split(':')[1:])
+        elif sentence.startswith('Ответ'):
+            answer = str(' '.join(sentence.split(':')[1:]))
+            questions_data[question] = answer
+    return questions_data
+
+
+def start(update: Update, context: CallbackContext):
     custom_keyboard = [["Новый вопрос", "Сдаться"],
                        ["Мой счёт"]]
     reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
@@ -17,18 +34,21 @@ def start(update: Update, context: CallbackContext) -> None:
     )
 
 
-def echo(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(update.message.text)
+def answer(update: Update, context: CallbackContext):
+    if update.message.text == "Новый вопрос":
+        question_rnd_index = random.randint(0, len(list(QUESTIONS_DATA))-1)
+        question = list(QUESTIONS_DATA.keys())[question_rnd_index]
+        update.message.reply_text(question)
 
 
-def main() -> None:
+def main():
     updater = Updater(os.getenv("TOKEN"))
 
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(CommandHandler("start", start))
 
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, answer))
 
     updater.start_polling()
 
@@ -37,4 +57,5 @@ def main() -> None:
 
 if __name__ == '__main__':
     load_dotenv()
+    QUESTIONS_DATA = collect_questions(os.getenv("QUESTIONS_PATH"))
     main()
